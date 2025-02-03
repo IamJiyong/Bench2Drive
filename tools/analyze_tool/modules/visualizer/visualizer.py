eda7021718c6fc1cd881030e336927f01774760b
IamJiyong <din2346@gmail.com>
2025-02-03 02:58:28 +0000

    Refactor visualizer to improve camera matrix loading and trajectory overlay handling

--- tools/analyze_tool/modules/visualizer/visualizer.py
+++ tools/analyze_tool/modules/visualizer/visualizer.py
@@ -12,7 +12,7 @@ import os
 import cv2
 import numpy as np
 from . import vis_utils
-from .vis_utils import VideoGenerator
+from .vis_utils import VideoGenerator, set_line_thickness
 from .camera_calibration import load_camera_config
 
 class Visualizer:
@@ -27,18 +27,25 @@ class Visualizer:
         self._config = config
         self.vis_elements = config.elements
         self.cameras_config = cameras_config  # Store camera configurations
-        self.camera_matrices = self._load_camera_matrices()
+        self.camera_matrices = self._load_camera_matrices(self._config.cameras)
 
 
-    def _load_camera_matrices(self):
+    def _load_camera_matrices(self, cam_names):
         """Load intrinsic and extrinsic matrices for all cameras."""
         camera_matrices = {}
-        for cam_name in self.cameras_config:
-            intrinsic, extrinsic = load_camera_config(self.cameras_config, cam_name)
-            camera_matrices[cam_name] = {
-                "intrinsic": intrinsic,
-                "extrinsic": extrinsic
-            }
+        for cam_name in cam_names:
+            lidar2img_list = self.cameras_config[cam_name].get('lidar2img_matrix', None)
+
+            if lidar2img_list is not None and False: # TODO: Not implemented yet
+                lidar2img_mat = np.array(lidar2img_list, dtype=np.float64)
+                camera_matrices[cam_name]['lidar2img_matrix'] = lidar2img_mat
+            else:
+                assert 'intrinsic_matrix' in self.cameras_config[cam_name], f"Missing intrinsic matrix for camera {cam_name}."
+                assert 'extrinsic_matrix' in self.cameras_config[cam_name], f"Missing extrinsic matrix for camera {cam_name}."
+
+                intrinsic_matrix, extrinsic_matrix = load_camera_config(self.cameras_config, cam_name)
+                camera_matrices[cam_name] = {"intrinsic": intrinsic_matrix, "extrinsic": extrinsic_matrix}
+
         return camera_matrices
 
 
@@ -64,52 +71,43 @@ class Visualizer:
                 continue
             
             # Retrieve intrinsic and extrinsic matrices for the current camera
-            intrinsic_matrix = self.camera_matrices[cam_name]["intrinsic"]
-            extrinsic_matrix = self.camera_matrices[cam_name]["extrinsic"]
+            matrices = self.camera_matrices[cam_name]
             
             # Overlay different visualization elements based on the specified vis_elements
             for element in self.vis_elements:
-                if element == "predicted_trajectory":
-                    # Overlay the predicted trajectory
-                    waypoints_2d = np.array(model_output["trajectories"], dtype=np.float32)
-                    waypoints_2d = waypoints_2d[:,:1,:,:]
-                    waypoints_2d = waypoints_2d.reshape(-1, waypoints_2d.shape[-2], 2)
+                thickness = set_line_thickness(cam_name, element)
+
+                if 'trajectory' in element:
+                    if element == "predicted_trajectory":
+                        # Load the predicted trajectory from the model output
+                        waypoints_2d = np.array(model_output["trajectories"], dtype=np.float32)
+                        waypoints_2d = waypoints_2d.reshape(-1,waypoints_2d.shape[-2], 2)
+
+                    elif element == "planned_trajectory":
+                        # Load the planned trajectory from the model output
+                        waypoints_2d = np.array(model_output["plan"], dtype=np.float32)
+                        waypoints_2d = waypoints_2d.reshape(1,-1,2)
 
                     num_traj = waypoints_2d.shape[0]
                     for i in range(num_traj):
+                        # TODO: hard-coded z-value (-1.6) for now
                         waypoints_3d = np.hstack((waypoints_2d[i], np.ones((waypoints_2d[i].shape[0], 1), dtype=np.float32) * (-1.6)))
                         image = vis_utils.overlay_trajectory(
-                            cam_name,
-                            element,
-                            image,
-                            waypoints_3d,
-                            intrinsic_matrix,
-                            extrinsic_matrix,
-                        )
-                elif element == "planned_trajectory":
-                    # Load the planned trajectory from the model output and add z-coordinate (0)
-                    waypoints_2d = np.array(model_output["plan"], dtype=np.float32)
-                    # TODO: hard-coded z-value (1.6) for now
-                    waypoints_3d = np.hstack((waypoints_2d, np.ones((waypoints_2d.shape[0], 1), dtype=np.float32) * (-1.6)))
+                            image=image,
+                            trajectory=waypoints_3d,
+                            matrices=matrices,
+                            thickness=thickness,
+                            is_ego=element=="planned_trajectory",
+                            )
 
-                    # Overlay the planned trajectory
-                    image = vis_utils.overlay_trajectory(
-                        cam_name,
-                        element,
-                        image,
-                        waypoints_3d,
-                        intrinsic_matrix,
-                        extrinsic_matrix,
-                    )
                 elif element == "bbox_3d":
                     # Draw bounding boxes
                     boxes_corners = model_output.get("boxes_corners", [])
                     image = vis_utils.draw_bounding_boxes(
-                        image,
-                        boxes_corners,
-                        cam_name=cam_name,
-                        camera_matrix=intrinsic_matrix,
-                        extrinsic_matrix=extrinsic_matrix,
+                        image=image,
+                        box_corners=boxes_corners,
+                        matrices=matrices,
+                        thickness=thickness,
                     )
             
             # Update the processed image in the dictionary
